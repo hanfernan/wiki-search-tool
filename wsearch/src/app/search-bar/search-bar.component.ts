6b36adcbd588669169a0674f20373c02f66909c6
Hannah Fernandes-Martin <hannah.fernandes-martin@liquidware.com>
2023-05-16 13:23:38 -0400

    added get request and basic table structure

--- wsearch/src/app/search-bar/search-bar.component.ts
+++ wsearch/src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent {
+  @Output() submitted = new EventEmitter<string>() ;
 
+  term = '';
+
+  onFormSubmit(event: any) {
+    event?.preventDefault()
+    this.submitted.emit(this.term);
+  }
 }
