f06a656327f3ccf9b92acb82f08aa24140785b1b
Hannah Fernandes-Martin <hannah.fernandes-martin@liquidware.com>
2023-05-17 16:12:17 -0400

    rxjs refactor

--- wsearch/src/app/app.component.ts
+++ wsearch/src/app/app.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { WikipediaService } from './wikipedia.service';
 
+interface Search {
+  title: string;
+  snippet: string;
+  pageid: number;
+ }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pages = [];
+  pages:Search[] = [];
 
   constructor(private wikipedia: WikipediaService) {}
 
   onTerm(term:string) {
-    this.wikipedia.search(term).subscribe((response: any) => {
-      this.pages = response.query.search;
+    this.wikipedia.search(term).subscribe((response) => {
+      //observer
+      this.pages = response;
     })
   }
 }
--- wsearch/src/app/wikipedia.service.ts
+++ wsearch/src/app/wikipedia.service.ts
@@ -1,5 +1,46 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { pluck} from 'rxjs/operators';
+//RxJS + TS example
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      title: string;
+      snippet: string;
+      pageid: number;
+    }[];
+  }
+}
+
+// interface Car {
+//   year: number;
+//   color: string;
+//   running: boolean;
+//   make: {
+//     name: string;
+//     dateCreated: number;
+//   }
+// }
+
+// const observable = new Observable<Car>((observer) => {
+//   observer.next({
+//     year: 2000,
+//     color: 'red',
+//     running: true,
+//     make: {
+//       name: 'Chevy',
+//       dateCreated: 1950
+//     }
+//   });
+// }).pipe(
+//   pluck('make', 'dateCreated')
+// );
+
+// observable.subscribe(value => {
+//   console.log(value);
+// });
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +49,20 @@ export class WikipediaService {
 
   constructor(private http: HttpClient) {}
 
-  public search(term:string) {
-    return this.http.get('https://en.wikipedia.org/w/api.php', {
-      params: {
-        action: 'query',
-        format: 'json',
-        list: 'search',
-        utf8: '1',
-        srsearch: term,
-        origin: '*'
-      }
-    });
+  public search(term: string) {
+    return this.http
+      .get<WikipediaResponse>('https://en.wikipedia.org/w/api.php', {
+      //observable
+        params: {
+          action: 'query',
+          format: 'json',
+          list: 'search',
+          utf8: '1',
+          srsearch: term,
+          origin: '*'
+        }
+     }).pipe(
+      pluck('query', 'search')
+    );
   }
 }
